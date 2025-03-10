ca5b9952470c9b9ee79333a332d4a2727b921e10
Nicholas A <nickis1329@gmail.com>
2025-03-10 09:30:49 -0700

    Spelling error fixed on .gitignore filename

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -6,37 +6,37 @@ const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
 const redirect_uris = [
-    "https://Your-meet-vercel-app-name.vercel.app/"
+  "https://Your-meet-vercel-app-name.vercel.app/"
 ];
 
 
 const oAuth2Client = new google.auth.OAuth2(
-    CLIENT_ID,
-    CLIENT_SECRET,
-    redirect_uris[0]
+  CLIENT_ID,
+  CLIENT_SECRET,
+  redirect_uris[0]
 );
 
 
 module.exports.getAuthURL = async () => {
-    /**
-     *
-     * Scopes array is passed to the `scope` option.
-     *
-     */
-    const authUrl = oAuth2Client.generateAuthUrl({
-        access_type: "offline",
-        scope: SCOPES,
-    });
+  /**
+   *
+   * Scopes array is passed to the `scope` option.
+   *
+   */
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
 
 
-    return {
-        statusCode: 200,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Credentials': true,
-        },
-        body: JSON.stringify({
-            authUrl,
-        }),
-    };
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify({
+      authUrl,
+    }),
+  };
 };
